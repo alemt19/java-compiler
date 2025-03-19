0f330171beb7c3509a844545b25f29fb2eb895c2
alemt19 <alfredomt1906@gmail.com>
2025-03-19 18:53:02 -0400

    textarea font

--- CodigoDePrueba.java
+++ CodigoDePrueba.java
@@ -1,10 +1,11 @@
 public class CodigoDePrueba {
     public static void main(String[] args) {
         int edad = 18;
+        String mensaje = "";
         if (edad >= 18) {
-            String mensaje = "Mayor de edad";
+            mensaje = "Mayor de edad";
         } else {
-            String mensaje = "Menor de edad";
+            mensaje = "Menor de edad";
         }
     }
 }

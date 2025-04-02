cefa0cf5386a15c2b42289b686fd8c46c9484c8c
alemt19 <alfredomt1906@gmail.com>
2025-04-02 10:41:21 -0400

    chore(parser): Update imports

--- src/parser/__init__.py
+++ src/parser/__init__.py
@@ -1,2 +1,2 @@
-from .parser import parse_code, get_errores_sint, crear_parser, get_ast_str
-from . code_generator import CodeGeneratorVisitor
+from parser.parser import parse_code, get_errores_sint, crear_parser, get_ast_str
+from parser.code_generator import CodeGeneratorVisitor
